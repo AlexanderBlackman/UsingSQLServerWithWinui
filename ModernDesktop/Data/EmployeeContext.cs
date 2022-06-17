9a26882c047d13ef04b2c3b1467954a23e6ba630
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-17 10:39:41 +1000

    Add project files.

---
+++ ModernDesktop/Data/EmployeeContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModernDesktop.Model;
+
+namespace ModernDesktop.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Employees;Integrated Security=True");
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    var id = 1;
+        //    foreach (var employee in InitialSeed.Seed("en", 25))
+        //    {
+        //        employee.Id = id++;
+        //        modelBuilder.Entity<Employee>().HasData(employee);
+        //    }
+        //}
+
+        internal DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ ModernDesktop/Services/EFDataService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModernDesktop.Contracts;
+using ModernDesktop.Data;
+using ModernDesktop.Model;
+
+namespace ModernDesktop.Services
+{
+    public class EFDataService : IDataService
+    {
+        private readonly EmployeeContext context;
+
+        public EFDataService(EmployeeContext dbContext)
+        {
+            this.context = dbContext;
+            this.context.Database.EnsureCreated();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return this.context.Employees.AsEnumerable();
+        }
+
+        public async Task<Employee> GetByIdAsync(int id)
+        {
+            return await this.context.Employees.Where(e => e.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpsertAsync(Employee employee)
+        {
+            //obviously this forbids last name changes
+            await this.context.Employees.Upsert(employee).On(e => new { e.FirstName, e.LastName }).RunAsync();
+
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var employee = await GetByIdAsync(id);
+            if (employee != null)
+            {
+                this.context.Employees.Remove(employee);
+                await this.context.SaveChangesAsync();
+            }
+            else
+            {
+                Debug.WriteLine($"Failed to delete id {id}");
+            }
+        }
+    }
+}
